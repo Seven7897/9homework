abbf976efaadf2274e010087d2bcec2c5e4a1f17
Anatoly Finchenko <finchenko777@gmail.com>
2022-07-26 18:06:37 +0300

    *first_commit*

---
+++ homework001/Program.cs
@@ -0,0 +1,11 @@
+﻿System.Console.WriteLine($"Диапазон цисел которые хотите найти от :");
+int first = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine("До :");
+int last = Convert.ToInt32(Console.ReadLine());
+string NumberRange(int n , int m)
+{
+    if (n == m)  return n.ToString();
+    return( n + "," +  NumberRange(n+1 , m));
+}
+
+System.Console.WriteLine(NumberRange(first,last));
